1f09d2118cb89f15a2ae7254bbb257f330c5efc0
Miguel Angel <25maikelo@gmail.com>
2024-02-29 21:33:39 -0600

    Basic Panel Structure

--- TI_1_Practica4.py
+++ TI_1_Practica4.py
@@ -1,16 +1,20 @@
 import panel as pn
-import panel.widgets as pnw
+import numpy as np
 import plotly.graph_objects as go
-
 from functions import bm_2d
 
-pn.extension('plotly')
 
 
-# Definir el widget para ingresar el número de pasos
-n_steps = pnw.IntInput(name='Number of steps:', value=20, step=10, start=1, end=1000)
+# Crear los widgets
+button_group1 = pn.widgets.RadioButtonGroup(name='Botones', options=['BM', 'CRW', 'LF'])
+number_of_steps_input = pn.widgets.IntInput(name='Number of steps', start=1, end=10000, value=1)
+start_x_pos_input = pn.widgets.FloatInput(name='Start X pos', start=0, end=100, value=0)
+start_y_pos_input = pn.widgets.FloatInput(name='Start Y pos', start=0, end=100, value=0)
+cauchy_coefficient_input = pn.widgets.FloatInput(name='Cauchy Coefficient', start=0.1, end=0.9, value=0.1, step=0.1)
+metrics_select = pn.widgets.Select(name='Metrics', options=['MSD', 'PL'])
 
-@pn.depends(n_steps)
+# Definir la función para graficar
+@pn.depends(number_of_steps_input)
 def plot_traj(n_steps):
 
   random_walker_df = bm_2d(n_steps)
@@ -26,5 +30,21 @@ def plot_traj(n_steps):
 
   return fig_traj_rw
 
-column = pn.Column(n_steps, plot_traj)
-column.show()
+# Organizar los widgets en grupos
+group1 = pn.Column(
+    pn.Row(button_group1),
+    pn.Column(number_of_steps_input, start_x_pos_input, start_y_pos_input, cauchy_coefficient_input),
+    pn.Row(metrics_select)
+)
+
+# Crear los paneles con las gráficas
+plot_panel1 = pn.Column(plot_traj)
+plot_panel2 = pn.Column(plot_traj)
+
+# Crear el layout final
+layout = pn.Column(
+    pn.Row(group1, plot_panel1, plot_panel2)
+)
+
+# Mostrar el panel
+layout.show()
