0fc98511d5283827196943f216678742e6d07443
Miguel Angel <25maikelo@gmail.com>
2024-03-03 00:05:21 -0600

    Add depedency of parameters with trajectory type

--- TI_1_Practica4.py
+++ TI_1_Practica4.py
@@ -1,20 +1,55 @@
 import panel as pn
 import numpy as np
 import plotly.graph_objects as go
-from functions import bm_2d, crw_2d, lf_2d
+from functions import bm_2d, crw_2d, lf_2d, get_mean_squared_displacement, get_step_lengths, get_turning_angles
 
+
+trajectory = []
 
 # Crear los widgets
+# TODO: Arreglar punto flotante
+# TODO: Decidir Metrica conforme el tipo de trayectoria
+# TODO: Verificar métrica
+# TODO: Acomodar trayectoria inicial vacía: no sé si debería estar como una variable externa fuera de las funciones
+
 trajectory_type = pn.widgets.RadioButtonGroup(name='Botones', options=['BM', 'CRW', 'LF'])
 number_of_steps_input = pn.widgets.IntInput(name='Number of steps', start=1, end=10000, value=100)
 speed_input = pn.widgets.FloatInput(name='Speed', start=1, end=100, value=6)
 start_x_pos_input = pn.widgets.FloatInput(name='Start X pos', start=0, end=100, value=0)
 start_y_pos_input = pn.widgets.FloatInput(name='Start Y pos', start=0, end=100, value=0)
+
 cauchy_coefficient_input = pn.widgets.FloatInput(name='Cauchy Coefficient', start=0.1, end=0.9, value=0.1, step=0.1)
+
 alpha_input = pn.widgets.FloatInput(name='Alpha', start=0.1, end=3, value=1, step=0.1)
 beta_input = pn.widgets.FloatInput(name='Beta', start=0.1, end=np.pi, value=1, step=0.1)
 loc_input = pn.widgets.FloatInput(name='Loc', start=0, end=100, value=0, step=0.1)
-metrics_select = pn.widgets.Select(name='Metrics', options=['MSD', 'PL'])
+
+metrics_select = pn.widgets.Select(name='Metrics', options=['MSD', 'PL', 'TA'])
+
+
+@pn.depends(metrics_select)
+def plot_metric(metrics_select):
+  if metrics_select == 'MSD':
+    metric_pd = get_mean_squared_displacement(trajectory)
+  elif metrics_select == 'PL':
+    metric_pd = get_step_lengths(trajectory)
+  elif metrics_select == 'TA':
+    metric_pd = get_turning_angles(trajectory)
+  else:
+    metric_pd = get_turning_angles(trajectory)
+
+  x_values = np.linspace(0, 100)
+  fig_traj_rw = go.Figure()
+  fig_traj_rw.add_trace(
+      go.Scatter(
+          x=x_values,
+          y=metric_pd,
+          name=f'Metric: {metrics_select}',
+          showlegend=True
+      )
+  )
+
+  return fig_traj_rw
 
 # Definir la función para graficar
 @pn.depends(
@@ -65,6 +100,8 @@ def plot_trajectory(
       speed=speed_input,
       s_pos=[start_x_pos_input, start_y_pos_input],
     )
+
+  trajectory = trajectory_df
   
   fig_trajectory = go.Figure()
   fig_trajectory.add_trace(
@@ -79,35 +116,53 @@ def plot_trajectory(
   fig_trajectory.update_layout(title=f'{trajectory_type} Trajectory', showlegend=True)
   return fig_trajectory
 
-@pn.depends(number_of_steps_input)
-def plot_metric(n_steps):
 
-  random_walker_df = bm_2d(n_steps)
-  fig_traj_rw = go.Figure()
-  fig_traj_rw.add_trace(
-      go.Scatter(
-          x=random_walker_df.x_pos,
-          y=random_walker_df.y_pos,
-          name=f'steps: {n_steps}',
-          showlegend=True
-      )
-  )
+@pn.depends(trajectory_type)
+def define_parameters(trajectory_type):
 
-  return fig_traj_rw
+  if trajectory_type == 'BM':
+    column = pn.Column(
+      number_of_steps_input,
+      start_x_pos_input,
+      start_y_pos_input,
+      speed_input,
+    )
+
+  elif trajectory_type == 'CRW':
+    column = pn.Column(
+      number_of_steps_input,
+      start_x_pos_input,
+      start_y_pos_input,
+      speed_input,
+      cauchy_coefficient_input,
+    )
+
+  elif trajectory_type == 'LF':
+    column = pn.Column(
+      number_of_steps_input,
+      start_x_pos_input,
+      start_y_pos_input,
+      speed_input,
+      cauchy_coefficient_input,
+      alpha_input,
+      beta_input,
+      loc_input,
+    )
+
+  else:
+    column = pn.Column(
+      number_of_steps_input,
+      start_x_pos_input,
+      start_y_pos_input,
+      speed_input,
+    )
+  
+  return column
 
 # Organizar los widgets en grupos
 group1 = pn.Column(
     pn.Row(trajectory_type),
-    pn.Column(
-      number_of_steps_input,
-      start_x_pos_input,
-      start_y_pos_input,
-      cauchy_coefficient_input,
-      speed_input,
-      alpha_input,
-      beta_input,
-      loc_input,
-    ),
+    define_parameters,
     pn.Row(metrics_select)
 )
 
